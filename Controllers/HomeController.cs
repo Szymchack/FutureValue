88fe8f2f566513b07448d33847d5f0e3476c28c2
Szymchack <44557214+Szymchack@users.noreply.github.com>
2021-11-16 11:35:25 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FutureValueReal.Models;
+public class HomeController : Controller 
+
+
+{ 
+        public IActionResult About()
+        {
+        return View();
+        }
+
+        public IActionResult Contact()
+        {
+        return View();
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index (FutureValueModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+        }
+        else
+        {
+            ViewBag.FV = 0; 
+        }
+        return View(model);
+    }
+        
+
+ 
+}
